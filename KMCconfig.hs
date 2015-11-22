f43a5842ba9b6bc307895dd0296736a570c31c3c
NaevaTheCat <rachaelnaeva@gmail.com>
2015-11-22 15:10:34 +1100

    automatable pass a float as an argument

--- KMCconfig.hs
+++ KMCconfig.hs
@@ -6,13 +6,13 @@ import qualified KMClattice as KL
 import qualified Data.HashMap as Map
 import qualified Data.Heap as H
 
-xdim = 50
-ydim = 50
+xdim = 10
+ydim = 10
 
 tEnd = 1.0
 tIncrement = 0.1
 
-pressureA = 0.55
+--pressureA = 0.55
 emptyState e = State (Site 0) e Empty 0
 occState e s = State Wild     e s     0
 co = Occupied 0
@@ -28,8 +28,8 @@ lattice = Lattice
             (V.replicate (xdim*ydim) 0.0)
             (KL.simpleCubic xdim ydim)
 
-rData = ReactionData
-            (V.fromList [o_ads, co_ads, o_co])
+rData pA = ReactionData
+            (V.fromList [o_ads pA, co_ads pA, o_co])
             (Map.empty)
             (V.empty)
             (V.replicate (xdim*ydim) [])
@@ -37,21 +37,21 @@ rData = ReactionData
             ([])
             (V.replicate (xdim*ydim) V.empty)
 
-o_ads = Reaction
+o_ads pA = Reaction
             (twoLinkedPoints)
             (twoLinkedPoints)
             (V.fromList [occState 0 vac, occState 1 vac])
             (V.fromList [occState 0 o, occState 1 o])
             (V.replicate 2 1)
-            (0.25*(1.0 - pressureA)*10)
+            (0.25*(1.0 - pA)*10)
             ([])
-co_ads = Reaction
+co_ads pA = Reaction
             (V.replicate 1 [])
             (V.replicate 1 [])
             (V.singleton (occState 0 vac))
             (V.singleton (occState 0 co))
             (V.singleton 0)
-            (10 * pressureA)
+            (10 * pA)
             ([])
 
 o_co = Reaction
